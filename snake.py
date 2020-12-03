bca5a31b0923c2b375029353dcedfb7c32c04be6
dave <dawid.strojek@gmail.com>
2020-12-03 16:43:00 +0100

    first version of best first search

--- snake.py
+++ snake.py
@@ -2,21 +2,23 @@ import pygame
 import sys
 import random
 import math
+import copy
+import time
 
 class Snake():
-    def __init__(self, type="walls"):
+    def __init__(self, type="walls", mode="taxi"):
         self.length = 1
         self.positions = [((screen_width/2), (screen_height/2))]
         self.direction = random.choice([up, down, left, right])
         self.color = (13, 250, 144)
-        # Special thanks to YouTubers Mini - Cafetos and Knivens Beast for raising this issue!
-        # Code adjustment courtesy of YouTuber Elija de Hoog
         self.score = 0
-        self.lives = 10000
+        self.lives = 1
         self.sum_score = 0
         self.time = 0
         self.sum_time = 0
         self.type = type
+        self.mode = mode
+        self.next_moves = []
 
     def get_head_position(self):
         return self.positions[0]
@@ -37,6 +39,20 @@ class Snake():
     def move(self):
         cur = self.get_head_position()
         x,y = self.direction
+        new = self.new_move(cur, x, y)
+        if (len(self.positions) > 2 and new in self.positions[2:]) or not (new[0] >= 0 and new[1] >= 0 and new[0] <= screen_width - gridsize and new[1] <= screen_height - gridsize):
+            self.lives -= 1
+            if self.lives > 0:
+                self.reset()
+            print("LOST")
+        else:
+            self.positions.insert(0,new)
+            if len(self.positions) > self.length:
+                self.positions.pop()
+
+    def move_to(self, direction):
+        cur = self.get_head_position()
+        x,y = direction
         new = self.new_move(cur, x, y)
         if (len(self.positions) > 2 and new in self.positions[2:]) or not (new[0] >= 0 and new[1] >= 0 and new[0] <= screen_width - gridsize and new[1] <= screen_height - gridsize):
             self.lives -= 1
@@ -80,89 +96,106 @@ class Snake():
     def get_possible_moves(self):
         moves = []
         head = self.get_head_position()
-        #print(self.positions)
         x,y = up
         new = self.new_move(head, x, y)
         if new not in self.positions and new[0] >= 0 and new[1] >= 0 and new[0] <= screen_width - gridsize and new[1] <= screen_height - gridsize:
-            #print("UP")
             moves.append(up)
         x,y = down
         new = self.new_move(head, x, y)
         if new not in self.positions and new[0] >= 0 and new[1] >= 0 and new[0] <= screen_width - gridsize and new[1] <= screen_height - gridsize:
-            #print("DOWN")
             moves.append(down)
         x,y = left
         new = self.new_move(head, x, y)
         if new not in self.positions and new[0] >= 0 and new[1] >= 0 and new[0] <= screen_width - gridsize and new[1] <= screen_height - gridsize:
-            #print("LEFT")
             moves.append(left)
         x,y = right
         new = self.new_move(head, x, y)
         if new not in self.positions and new[0] >= 0 and new[1] >= 0 and new[0] <= screen_width - gridsize and new[1] <= screen_height - gridsize:
-            #print("RIGHT")
             moves.append(right)
         return moves
 
-    def heuristic_move(self, food, mode):
-        moves = self.get_possible_moves()
-        head = self.get_head_position()
-        bestMove = 0
+    def heuristic_move(self, food):
+        node = Node(food, self, None, None)
+        bestChildren = []
         distance = 10000000
-        #print(moves)
-        if len(moves) != 0:
-            if mode == "random":
-                bestMoves = moves
-            if self.type == "walls":
-                if mode == "euclides":
-                    for move in moves:
-                        x, y = move
-                        new = self.new_move(head, x, y)
-                        if math.sqrt((new[0] - food.position[0])**2 + (new[1] - food.position[1])**2) < distance:
-                            distance = math.sqrt((new[0] - food.position[0])**2 + (new[1] - food.position[1])**2)
-                            bestMoves = [move]
-                if mode == "taxi":
-                    for move in moves:
-                        x, y = move
-                        new = self.new_move(head, x, y)
-                        if abs(new[0] - food.position[0]) + abs(new[1] - food.position[1]) < distance:
-                            distance = abs(new[0] - food.position[0]) + abs(new[1] - food.position[1])
-                            bestMoves = [move]
-                if mode == "taxirandom":
-                    for move in moves:
-                        x, y = move
-                        new = self.new_move(head, x, y)
-                        if abs(new[0] - food.position[0]) + abs(new[1] - food.position[1]) < distance:
-                            distance = abs(new[0] - food.position[0]) + abs(new[1] - food.position[1])
-                            bestMoves = [move]
-                        if abs(new[0] - food.position[0]) + abs(new[1] - food.position[1]) == distance:
-                            bestMoves.append(move)
+        if len(node.get_children()) != 0:
+            if self.mode == "taxi":
+                for child in node.get_children():
+                    child.distance_to_food()
+                    if child.to_food < distance:
+                        distance = child.to_food
+                        bestChildren = [child]
+            elif self.mode == "euclides":
+                for child in node.get_children():
+                    child.distance_to_food()
+                    if child.to_food < distance:
+                        distance = child.to_food
+                        bestChildren = [child]
+            elif self.mode == "taxirandom":
+                for child in node.get_children():
+                    child.distance_to_food()
+                    if child.to_food < distance:
+                        distance = child.to_food
+                        bestChildren = [child]
+                    if child.to_food == distance:
+                        bestChildren.append(child)
+            elif self.mode == "best first search":
+                if len(self.next_moves) == 0:
+                    start_node = node
+                    new_directions = [[(0,-1), 0], [(0,1), 0], [(-1,0), 0], [(1,0), 0]]
+                    open = [node]
+                    closed = []
+                    timeout = time.time() + 0.1
+                    while len(open) != 0 and time.time() < timeout:
+                        open.sort()
+                        current_node = open.pop(0)
+                        closed.append(current_node)
+                        current_node.distance_to_food()
+                        if current_node.to_food == 0:
+                            path = []
+                            while current_node != start_node:
+                                path.append(current_node)
+                                current_node = current_node.parent
+                            #path.append(start_node)
+                            # Return reversed path
+                            self.next_moves =  path[::-1]
+                            break
+                        if len(current_node.get_children()) == 0:
+                            path = []
+                            while current_node != start_node:
+                                path.append(current_node)
+                                current_node = current_node.parent
+                                next_moves =  path[::-1]
+                            for new_direction in new_directions:
+                                if next_moves[0].direction == new_direction[0]:
+                                    if len(next_moves) < new_direction[1]:
+                                        new_direction[1] = -len(next_moves)
 
-            elif self.type == "no walls":
-                if mode == "euclides":
-                    for move in moves:
-                        x, y = move
-                        new = self.new_move(head, x, y)
-                        if math.sqrt(min((new[0] - food.position[0] - screen_width)**2, (new[0] - food.position[0])**2, (new[0] - food.position[0] + screen_width)**2) + min((new[1] - food.position[1] - screen_height)**2, (new[1] - food.position[1])**2, (new[1] - food.position[1] + screen_height)**2)) < distance:
-                            distance = math.sqrt(min((new[0] - food.position[0] - screen_width)**2, (new[0] - food.position[0])**2, (new[0] - food.position[0] + screen_width)**2) + min((new[1] - food.position[1] - screen_height)**2, (new[1] - food.position[1])**2, (new[1] - food.position[1] + screen_height)**2))
-                            bestMoves = [move]
-                if mode == "taxi":
-                    for move in moves:
-                        x, y = move
-                        new = self.new_move(head, x, y)
-                        if min(abs(new[0] - food.position[0] - screen_width), abs(new[0] - food.position[0]), abs(new[0] - food.position[0] + screen_width)) + min(abs(new[1] - food.position[1] - screen_height), abs(new[1] - food.position[1]), abs(new[1] - food.position[1] + screen_height)) < distance:
-                            distance = min(abs(new[0] - food.position[0] - screen_width), abs(new[0] - food.position[0]), abs(new[0] - food.position[0] + screen_width)) + min(abs(new[1] - food.position[1] - screen_height), abs(new[1] - food.position[1]), abs(new[1] - food.position[1] + screen_height))
-                            bestMoves = [move]
-                if mode == "taxirandom":
-                    for move in moves:
-                        x, y = move
-                        new = self.new_move(head, x, y)
-                        if min(abs(new[0] - food.position[0] - screen_width), abs(new[0] - food.position[0]), abs(new[0] - food.position[0] + screen_width)) + min(abs(new[1] - food.position[1] - screen_height), abs(new[1] - food.position[1]), abs(new[1] - food.position[1] + screen_height)) < distance:
-                            distance = min(abs(new[0] - food.position[0] - screen_width), abs(new[0] - food.position[0]), abs(new[0] - food.position[0] + screen_width)) + min(abs(new[1] - food.position[1] - screen_height), abs(new[1] - food.position[1]), abs(new[1] - food.position[1] + screen_height))
-                            bestMoves = [move]
-                        if min(abs(new[0] - food.position[0] - screen_width), abs(new[0] - food.position[0]), abs(new[0] - food.position[0] + screen_width)) + min(abs(new[1] - food.position[1] - screen_height), abs(new[1] - food.position[1]), abs(new[1] - food.position[1] + screen_height)) == distance:
-                            bestMoves.append(move)
+                        for child in current_node.get_children():
+                            if child in closed:
+                                continue
+                            child.distance_to_food()
+                            if child not in open:
+                                open.append(child)
 
-            self.turn(random.choice(bestMoves))
+                    best = 10000000
+                    print new_directions
+                    for new_direction in new_directions:
+                        if new_direction[1] != -10000:
+                            if new_direction[1] < best:
+                                best = new_direction[1]
+                                bestChildren = [Node(food, self, None, new_direction[0])]
+                    if len(self.next_moves) == 0:
+                        bestChildren = [Node(food, self, None, random.choice(self.get_possible_moves()))]
+
+            if len(self.next_moves) == 0:
+                bestChild = random.choice(bestChildren)
+                self.move_to(bestChild.direction)
+            else:
+                self.move_to(self.next_moves[0].direction)
+                self.next_moves.pop(0)
+        else:
+            self.reset()
 
 class Food():
     def __init__(self, snake):
@@ -190,10 +223,66 @@ def drawGrid(surface):
                 rr = pygame.Rect((x*gridsize, y*gridsize), (gridsize,gridsize))
                 pygame.draw.rect(surface, (0,0,0), rr)
 
-screen_width = 800
-screen_height = 800
+class Node():
+    def __init__(self, food, snake, parent, direction, from_start=0):
+        self.parent = parent
+        self.snake = snake
+        self.food = food
+        self.direction = direction
+        self.from_start = from_start
+        self.to_food = None
+        self.f = None
+        self.deaths = 0
 
-gridsize = 10
+    def get_snake(self):
+        return copy.deepcopy(self.snake)
+
+        # Compare nodes
+    def __eq__(self, other):
+        return self.snake.positions == other.snake.positions
+    # Sort nodes
+    def __lt__(self, other):
+         return self.to_food + self.from_start < other.to_food + other.from_start
+
+    def taxi(self):
+        node_head = self.snake.get_head_position()
+        node_food = self.food.position
+        if self.snake.type == "no walls":
+            return min(abs(node_head[0] - node_food[0] - screen_width), abs(node_head[0] - node_food[0]), abs(node_head[0] - node_food[0] + screen_width)) + min(abs(node_head[1] - node_food[1] - screen_height), abs(node_head[1] - node_food[1]), abs(node_head[1] - node_food[1] + screen_height))
+        elif self.snake.type == "walls":
+            return abs(node_head[0] - node_food[0])+ abs(node_head[1] - node_food[1])
+
+    def euclides(self):
+        node_head = self.snake.get_head_position()
+        node_food = self.food.position
+        if self.snake.type == "no walls":
+            return math.sqrt(min((node_head[0] - node_food[0] - screen_width)**2, (node_head[0] - node_food[0])**2, (node_head[0] - node_food[0] + screen_width)**2) + min((node_head[1] - node_food[1] - screen_height)**2, (node_head[1] - node_food[1])**2, (node_head[1] - node_food[1] + screen_height)**2))
+        elif self.snake.type == "walls":
+            return math.sqrt((node_head[0] - node_food[0])**2 + (node_head[1] - node_food[1])**2)
+
+    def distance_to_food(self):
+        if self.snake.mode == "euclides" or self.snake.mode == "best first search":
+            self.to_food = self.euclides()
+        elif self.snake.mode == "taxi" or self.snake.mode == "taxirandom":
+            self.to_food = self.taxi()
+        self.f = self.to_food + self.from_start
+
+    def get_children(self):
+        directions = self.snake.get_possible_moves()
+        array_of_children = []
+        for direction in directions:
+            new_snake = self.get_snake()
+            new_snake.move_to(direction)
+            array_of_children.append(Node(self.food, new_snake, self, direction, self.from_start + 1))
+        return array_of_children
+
+    def update_deaths():
+        self.deaths += 1
+
+screen_width = 600
+screen_height = 600
+
+gridsize = 20
 grid_width = screen_width/gridsize
 grid_height = screen_height/gridsize
 
@@ -212,17 +301,17 @@ def main():
     surface = surface.convert()
     drawGrid(surface)
 
-    snake = Snake("no walls")
+    snake = Snake("walls", "best first search")
     food = Food(snake)
 
     myfont = pygame.font.SysFont("monospace",16)
 
     while (True):
-        clock.tick(50)
+        #clock.tick(10)
         snake.handle_keys()
         drawGrid(surface)
-        snake.heuristic_move(food, "taxi")
-        snake.move()
+        snake.heuristic_move(food)
+        #snake.move()
         if snake.get_head_position() == food.position:
             snake.length += 1
             snake.score += 1
@@ -233,8 +322,8 @@ def main():
         snake.draw(surface)
         food.draw(surface)
         screen.blit(surface, (0,0))
-        #score = myfont.render("Score {0}".format(snake.score), 1, (255,255,255))
-        #screen.blit(score, (5,10))
+        score = myfont.render("Score {0}".format(snake.score), 1, (255,255,255))
+        screen.blit(score, (5,10))
         #lives = myfont.render("Lives {0}".format(snake.lives), 1, (255,255,255))
         #screen.blit(lives, (screen_width - 100,10))
         #time = myfont.render("Time {0}".format(snake.time), 1, (255,255,255))
