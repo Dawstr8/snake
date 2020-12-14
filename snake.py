822fbeaead5ff8b667d75b9e67264bd745d99229
dave <dawid.strojek@gmail.com>
2020-12-14 18:52:36 +0100

    base hamiltonian path cutting

--- snake.py
+++ snake.py
@@ -8,7 +8,7 @@ import time
 class Snake():
     def __init__(self, type="walls", mode="taxi"):
         self.length = 1
-        self.positions = [((screen_width/2), (screen_height/2))]
+        self.positions = [(grid_width/2, grid_height/2)]
         self.direction = random.choice([up, down, left, right])
         self.color = (13, 250, 144)
         self.score = 0
@@ -31,16 +31,18 @@ class Snake():
 
     def new_move(self, cur, x, y):
         if self.type == "no walls":
-            new = (((cur[0]+(x*gridsize))%screen_width), (cur[1]+(y*gridsize))%screen_height)
+            new = (((cur[0]+x)%grid_width), (cur[1]+y)%grid_height)
+            #new = (((cur[0]+(x*gridsize))%screen_width), (cur[1]+(y*gridsize))%screen_height)
         elif self.type == "walls":
-            new = (((cur[0]+(x*gridsize))), (cur[1]+(y*gridsize)))
+            new = ((cur[0]+x), (cur[1]+y))
+            #new = (((cur[0]+(x*gridsize))), (cur[1]+(y*gridsize)))
         return new
 
     def move(self):
         cur = self.get_head_position()
         x,y = self.direction
         new = self.new_move(cur, x, y)
-        if (len(self.positions) > 2 and new in self.positions[2:]) or not (new[0] >= 0 and new[1] >= 0 and new[0] <= screen_width - gridsize and new[1] <= screen_height - gridsize):
+        if (len(self.positions) > 2 and new in self.positions[2:]) or not (new[0] >= 0 and new[1] >= 0 and new[0] < grid_width and new[1] < grid_height):
             self.lives -= 1
             if self.lives > 0:
                 self.reset()
@@ -54,7 +56,7 @@ class Snake():
         cur = self.get_head_position()
         x,y = direction
         new = self.new_move(cur, x, y)
-        if (len(self.positions) > 2 and new in self.positions[2:]) or not (new[0] >= 0 and new[1] >= 0 and new[0] <= screen_width - gridsize and new[1] <= screen_height - gridsize):
+        if (len(self.positions) > 2 and new in self.positions[2:]) or not (new[0] >= 0 and new[1] >= 0 and new[0] < grid_width and new[1] < grid_height):
             self.lives -= 1
             if self.lives > 0:
                 self.reset()
@@ -67,14 +69,15 @@ class Snake():
 
     def reset(self):
         self.length = 1
-        self.positions = [((screen_width/2), (screen_height/2))]
+        self.positions = [(grid_width/2, grid_height/2)]
         self.direction = random.choice([up, down, left, right])
         self.score = 0
         self.time = 0
 
     def draw(self,surface):
         for p in self.positions:
-            r = pygame.Rect((p[0], p[1]), (gridsize,gridsize))
+            r = pygame.Rect((p[0]*gridsize, p[1]*gridsize), (gridsize,gridsize))
+            #r = pygame.Rect((p[0], p[1]), (gridsize,gridsize))
             pygame.draw.rect(surface, self.color, r)
             pygame.draw.rect(surface, (0,0, 0), r, 1)
 
@@ -98,19 +101,19 @@ class Snake():
         head = self.get_head_position()
         x,y = up
         new = self.new_move(head, x, y)
-        if new not in self.positions and new[0] >= 0 and new[1] >= 0 and new[0] <= screen_width - gridsize and new[1] <= screen_height - gridsize:
+        if new not in self.positions and new[0] >= 0 and new[1] >= 0 and new[0] < grid_width and new[1] < grid_height:
             moves.append(up)
         x,y = down
         new = self.new_move(head, x, y)
-        if new not in self.positions and new[0] >= 0 and new[1] >= 0 and new[0] <= screen_width - gridsize and new[1] <= screen_height - gridsize:
+        if new not in self.positions and new[0] >= 0 and new[1] >= 0 and new[0] < grid_width and new[1] < grid_height:
             moves.append(down)
         x,y = left
         new = self.new_move(head, x, y)
-        if new not in self.positions and new[0] >= 0 and new[1] >= 0 and new[0] <= screen_width - gridsize and new[1] <= screen_height - gridsize:
+        if new not in self.positions and new[0] >= 0 and new[1] >= 0 and new[0] < grid_width and new[1] < grid_height:
             moves.append(left)
         x,y = right
         new = self.new_move(head, x, y)
-        if new not in self.positions and new[0] >= 0 and new[1] >= 0 and new[0] <= screen_width - gridsize and new[1] <= screen_height - gridsize:
+        if new not in self.positions and new[0] >= 0 and new[1] >= 0 and new[0] < grid_width and new[1] < grid_height:
             moves.append(right)
         return moves
 
@@ -139,6 +142,54 @@ class Snake():
                         best_children = [child]
                     if child.to_food == distance:
                         best_children.append(child)
+            elif self.mode == "hamilton":
+                for i in range(len(hamiltonian_cycle)):
+                    if hamiltonian_cycle[i] == self.get_head_position():
+                        head = i
+                    if hamiltonian_cycle[i] == food.position:
+                        food_position = i
+                child = Node(food, self, None, (hamiltonian_cycle[(head+1)%(grid_width*grid_height)][0]-hamiltonian_cycle[head][0], hamiltonian_cycle[(head+1)%(grid_width*grid_height)][1] - hamiltonian_cycle[head][1]))
+                best_children.append(child)
+            elif self.mode == "hamilton with cut":
+                head = self.get_head_position()
+                children = []
+                for move in self.get_possible_moves():
+                    children.append([(head[0] + move[0], head[1] + move[1]), None])
+                for i in range(len(hamiltonian_cycle)):
+                    if hamiltonian_cycle[i] == self.get_head_position():
+                        head_position = i
+                    if hamiltonian_cycle[i] == food.position:
+                        food_position = i
+                    for child in children:
+                        if hamiltonian_cycle[i] == child[0]:
+                            child[1] = i
+                #if food_position < head_position:
+                #    best_child = Node(food, self, None, (hamiltonian_cycle[(head_position+1)%(grid_width*grid_height)][0]-hamiltonian_cycle[head_position][0], hamiltonian_cycle[(head_position+1)%(grid_width*grid_height)][1] - hamiltonian_cycle[head_position][1]))
+                if self.length < 700:
+                    best_child = Node(food, self, None, (hamiltonian_cycle[(head_position+1)%(grid_width*grid_height)][0]-hamiltonian_cycle[head_position][0], hamiltonian_cycle[(head_position+1)%(grid_width*grid_height)][1] - hamiltonian_cycle[head_position][1]))
+                    best = (food_position - head_position - 1)%(grid_width*grid_height)
+                    for child in children:
+                        distance_to_food = (food_position - child[1])%(grid_width*grid_height)
+                        distance_to_head = (head_position - child[1])%(grid_width*grid_height)
+                        if distance_to_food < best:
+                            can_go = True
+                            for i in range((child[1] - head_position)%(grid_width*grid_height)):
+                                print i, hamiltonian_cycle[(i + child[1])%(grid_width*grid_height)]
+                                if hamiltonian_cycle[(i + child[1])%(grid_width*grid_height)] in self.positions:
+                                    can_go = False
+                                    break
+                            if can_go:
+                                best = distance_to_food
+                                best_child = Node(food, self, None, (hamiltonian_cycle[child[1]][0]-hamiltonian_cycle[head_position][0], hamiltonian_cycle[child[1]][1] - hamiltonian_cycle[head_position][1]))
+                else:
+                    for i in range(len(hamiltonian_cycle)):
+                        if hamiltonian_cycle[i] == self.get_head_position():
+                            head = i
+                        if hamiltonian_cycle[i] == food.position:
+                            food_position = i
+                    best_child = Node(food, self, None, (hamiltonian_cycle[(head+1)%(grid_width*grid_height)][0]-hamiltonian_cycle[head][0], hamiltonian_cycle[(head+1)%(grid_width*grid_height)][1] - hamiltonian_cycle[head][1]))
+                best_children.append(best_child)
+
             elif self.mode == "euclides best first search" or self.mode == "taxi best first search":
                 new_directions = [[(0,-1), 0], [(0,1), 0], [(-1,0), 0], [(1,0), 0]]
                 if len(self.next_moves) == 0:
@@ -214,12 +265,13 @@ class Food():
         self.randomize_position(snake)
 
     def randomize_position(self, snake):
-        self.position = (random.randint(0, grid_width-1)*gridsize, random.randint(0, grid_height-1)*gridsize)
+        self.position = (random.randint(0, grid_width-1), random.randint(0, grid_height-1))
         while self.position in snake.positions:
-            self.position = (random.randint(0, grid_width-1)*gridsize, random.randint(0, grid_height-1)*gridsize)
+            self.position = (random.randint(0, grid_width-1), random.randint(0, grid_height-1))
+            #self.position = (random.randint(0, grid_width-1)*gridsize, random.randint(0, grid_height-1)*gridsize)
 
     def draw(self, surface):
-        r = pygame.Rect((self.position[0], self.position[1]), (gridsize, gridsize))
+        r = pygame.Rect((self.position[0]*gridsize, self.position[1]*gridsize), (gridsize, gridsize))
         pygame.draw.rect(surface, self.color, r)
         pygame.draw.rect(surface, (0, 0, 0), r, 1)
 
@@ -234,7 +286,7 @@ def drawGrid(surface):
                 pygame.draw.rect(surface, (0,0,0), rr)
 
 class Node():
-    def __init__(self, food, snake, parent, direction, from_start=0):
+    def __init__(self, food, snake, parent, direction, from_start=0, first_direction=None):
         self.parent = parent
         self.snake = snake
         self.food = food
@@ -242,6 +294,7 @@ class Node():
         self.from_start = from_start
         self.to_food = None
         self.deaths = 0
+        self.first_direction = first_direction
 
     def get_snake(self):
         return copy.deepcopy(self.snake)
@@ -251,13 +304,13 @@ class Node():
         return self.snake.positions == other.snake.positions
     # Sort nodes
     def __lt__(self, other):
-         return self.to_food + self.from_start < other.to_food + other.from_start
+         return self.f < other.f
 
     def taxi(self):
         node_head = self.snake.get_head_position()
         node_food = self.food.position
         if self.snake.type == "no walls":
-            return min(abs(node_head[0] - node_food[0] - screen_width), abs(node_head[0] - node_food[0]), abs(node_head[0] - node_food[0] + screen_width)) + min(abs(node_head[1] - node_food[1] - screen_height), abs(node_head[1] - node_food[1]), abs(node_head[1] - node_food[1] + screen_height))
+            return min(abs(node_head[0] - node_food[0] - grid_width), abs(node_head[0] - node_food[0]), abs(node_head[0] - node_food[0] + grid_width)) + min(abs(node_head[1] - node_food[1] - grid_height), abs(node_head[1] - node_food[1]), abs(node_head[1] - node_food[1] + grid_height))
         elif self.snake.type == "walls":
             return abs(node_head[0] - node_food[0])+ abs(node_head[1] - node_food[1])
 
@@ -265,7 +318,7 @@ class Node():
         node_head = self.snake.get_head_position()
         node_food = self.food.position
         if self.snake.type == "no walls":
-            return math.sqrt(min((node_head[0] - node_food[0] - screen_width)**2, (node_head[0] - node_food[0])**2, (node_head[0] - node_food[0] + screen_width)**2) + min((node_head[1] - node_food[1] - screen_height)**2, (node_head[1] - node_food[1])**2, (node_head[1] - node_food[1] + screen_height)**2))
+            return math.sqrt(min((node_head[0] - node_food[0] - grid_width)**2, (node_head[0] - node_food[0])**2, (node_head[0] - node_food[0] + grid_width)**2) + min((node_head[1] - node_food[1] - grid_height)**2, (node_head[1] - node_food[1])**2, (node_head[1] - node_food[1] + grid_height)**2))
         elif self.snake.type == "walls":
             return math.sqrt((node_head[0] - node_food[0])**2 + (node_head[1] - node_food[1])**2)
 
@@ -274,7 +327,7 @@ class Node():
             self.to_food = self.euclides()
         elif self.snake.mode == "taxi" or self.snake.mode == "taxirandom" or self.snake.mode == "taxi best first search":
             self.to_food = self.taxi()
-        self.f = self.to_food + self.from_start
+        self.f = self.to_food
 
     def get_children(self):
         directions = self.snake.get_possible_moves()
@@ -285,12 +338,25 @@ class Node():
             array_of_children.append(Node(self.food, new_snake, self, direction, self.from_start + 1))
         return array_of_children
 
+
+
 screen_width = 600
 screen_height = 600
 
 gridsize = 20
 grid_width = screen_width/gridsize
 grid_height = screen_height/gridsize
+
+hamiltonian_cycle = []
+for i in range(grid_width):
+    hamiltonian_cycle.append((0, grid_width - 1 - i))
+for i in range(grid_width):
+    if (i + 1) % 2 == 0:
+        for j in range(grid_height - 1):
+            hamiltonian_cycle.append((grid_width - 1 - j, i))
+    else:
+        for j in range(grid_height - 1):
+            hamiltonian_cycle.append((j + 1, i))
 
 up = (0,-1)
 down = (0,1)
@@ -299,7 +365,6 @@ right = (1,0)
 
 def main():
     pygame.init()
-
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
 
@@ -307,13 +372,14 @@ def main():
     surface = surface.convert()
     drawGrid(surface)
 
-    snake = Snake("walls", "euclides best first search")
+    snake = Snake("walls", "hamilton with cut")
     food = Food(snake)
+    #snake.heuristic_move(food)
 
     myfont = pygame.font.SysFont("monospace",16)
 
     while (True):
-        #clock.tick(20)
+        #clock.tick(1000)
         snake.handle_keys()
         drawGrid(surface)
         snake.heuristic_move(food)
